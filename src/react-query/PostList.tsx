a98dba7dbd76c0a54b1964d78a154164ba7f4ff3
Akash Padampalle <akashpadampalle780@gmail.com>
2023-06-18 17:49:47 +0530

    learn pagination

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -2,23 +2,14 @@ import { useState } from "react";
 import usePost from "./hooks/usePosts";
 
 const PostList = () => {
-  const [userId, setUerId] = useState<number>();
-  const { data: posts, error } = usePost(userId);
+  const pageSize = 10;
+  const [page, setPage] = useState(1);
+  const { data: posts, error } = usePost({ page, pageSize });
 
   if (error) return <p>{error.message}</p>;
 
   return (
     <>
-      <select
-        onChange={(event) => setUerId(parseInt(event.target.value))}
-        value={userId}
-        className="form-select mb-3"
-      >
-        <option value=""></option>
-        <option value="1">user 1</option>
-        <option value="2">user 2</option>
-        <option value="3">user 3</option>
-      </select>
       <ul className="list-group">
         {posts?.map((post) => (
           <li key={post.id} className="list-group-item">
@@ -26,6 +17,19 @@ const PostList = () => {
           </li>
         ))}
       </ul>
+      <button
+        className="btn btn-primary mt-3"
+        disabled={page === 1}
+        onClick={() => setPage(page - 1)}
+      >
+        Previous
+      </button>
+      <button
+        className="btn btn-primary mt-3 ms-3"
+        onClick={() => setPage(page + 1)}
+      >
+        Next
+      </button>
     </>
   );
 };
